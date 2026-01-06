a3b0c106a5e65bc6210ebe8adb9648af67dd4bf1
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2026-01-06 11:43:08 -0800

    navbar links styled for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,25 +4,29 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-content">
-        <Link to="/">
-          <img
-            src="/images/accrossthecreek_logo.png"
-            alt="Across The Creek Organics Logo"
-            className="atco-logo"
-          />
-        </Link>
-        <h1>Across The Creek Organics</h1>
-        <ul className="nav-links">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact Form</Link>
-          </li>
-        </ul>
+        <div className="logo-and-title">
+          <Link to="/">
+            <img
+              src="/images/accrossthecreek_logo.png"
+              alt="Across The Creek Organics Logo"
+              className="atco-logo"
+            />
+          </Link>
+          <h1>Across The Creek Organics</h1>
+        </div>
+        <div className="nav-links-container">
+          <ul className="nav-links">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Form</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
