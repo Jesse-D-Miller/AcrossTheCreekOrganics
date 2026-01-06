5385b03beef62f60e6e0a2f8266f3fb7165be1a4
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2026-01-06 11:08:15 -0800

    product cards adjusted from grid to flex containers

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,38 +2,35 @@ import useCart from "../context/useCart";
 import { useState } from "react";
 
 function Product({ name, description, image }) {
-
-
   return (
-    <div
-      className="product"
-      style={{
-        backgroundImage: `url(${image})`,
-      }}
-    >
-        <div
-          className="product-content"
-          style={{ position: "relative", zIndex: 2 }}
-        >
-          <h5>{name}</h5>
-          <p>{description}</p>
-        </div>
+    <div className="product">
+      <div
+        className="product-image"
+        style={{
+          backgroundImage: `url(${image})`,
+        }}
+      ></div>
+      <div
+        className="product-content"
+        style={{ position: "relative", zIndex: 2 }}
+      >
+        <h5>{name}</h5>
+        <p>{description}</p>
+      </div>
 
-        {/* image overlay, dark */}
-        <div
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            background: "rgba(0,0,0,0.3)",
-            zIndex: 1,
-            borderRadius: "2px",
-          }}
-        />
-
-
+      {/* image overlay, dark
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          background: "rgba(0,0,0,0.3)",
+          zIndex: 1,
+          borderRadius: "2px",
+        }}
+      /> */}
     </div>
   );
 }
