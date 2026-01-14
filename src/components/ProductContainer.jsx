b6e7df493844a95ce3d1aed55bf9ef4bfd1621e8
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2026-01-14 13:08:40 -0800

    small preamble added to top of front page to explain how orders are placed

--- src/components/ProductContainer.jsx
+++ src/components/ProductContainer.jsx
@@ -5,6 +5,14 @@ import { Link } from "react-router-dom";
 function ProductContainer() {
   return (
     <>
+      <div className="order-preamble">
+        <p>
+          <strong>About Ordering: </strong>
+          This website allows you to submit an order request. Availability is
+          confirmed after we review each request. We’ll follow up with
+          confirmation, pricing, and shipping details.
+        </p>
+      </div>
       <div className="product-container">
         {potatoData.map((potato) => (
           <Link
