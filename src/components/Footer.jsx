8cebdeb31ea40f8352af7254f607a59b027e5cad
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2026-01-16 15:06:23 -0800

    more styling for the footer and a tags added to make contact info interactive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Footer() {
   return (
     <div className="footer">
@@ -8,11 +10,29 @@ function Footer() {
           className="atco-logo"
         />
         <div className="footer-text">
-          <p>Across The Creek Organics</p>
-          <p>8324 Pemberton Meadows Rd</p>
-          <p>Pemberton BC</p>
-          <p>​604-698-8137</p>
-          <p>acrossthecreek@gmail.com</p>
+          <a
+            href="https://www.google.com/maps/search/?api=1&query=Across+The+Creek+Organics,+Pemberton+BC"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "inherit", textDecoration: "none" }}
+          >
+            <p>Across The Creek Organics</p>
+            <p>8324 Pemberton Meadows Rd,</p>
+            <p>Pemberton BC</p>
+          </a>
+          <p>
+            <a href="tel:604-698-8137" style={{ color: "inherit" }}>
+              604-698-8137
+            </a>
+          </p>
+          <p>
+            <a
+              href="mailto:acrossthecreek@gmail.com"
+              style={{ color: "inherit" }}
+            >
+              acrossthecreek@gmail.com
+            </a>
+          </p>
         </div>
       </div>
     </div>
